0b74f38df1cb4f46062df3ca3520c1fbaefd3045
xuanjun888 <2533348564@qq.com>
2020-10-28 23:32:23 +0800

    req #0 init

---
+++ src/main/java/Application.java
@@ -0,0 +1,12 @@
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableAutoConfiguration
+@SpringBootApplication
+@ComponentScan
+@MapperScan("com.roncoo.eshop.inventory.mapper")
+public class Application {
+
+}
