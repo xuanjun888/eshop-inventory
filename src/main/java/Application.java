7c8c81e18fb91b991662c9335941f527531aa9f1
xuanjun <xuanjun@purang.com>
2020-10-29 17:42:33 +0800

    req #0

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,12 +1,17 @@
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
 @SpringBootApplication
 @ComponentScan
 @MapperScan("com.roncoo.eshop.inventory.mapper")
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 }
