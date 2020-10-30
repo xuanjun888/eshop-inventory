afdf5e25fadcab0fb3b63a235efbbb15a0bc6e4d
xuanjun <xuanjun@purang.com>
2020-10-30 17:24:00 +0800

    req #0

--- src/main/java/com/runcoo/eshop/inventory/Application.java
+++ src/main/java/com/runcoo/eshop/inventory/Application.java
@@ -1,8 +1,10 @@
+package com.runcoo.eshop.inventory;
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
